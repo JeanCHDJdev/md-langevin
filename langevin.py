a3768ec89d9aeed412d2fe3e1aaadb690abe2586
JeanCHDJdev <jean.choppindejanvrydev@gmail.com>
2025-10-27 14:24:28 +0100

    updated simulation with langevin and notebook

--- langevin.py
+++ langevin.py
@@ -5,13 +5,13 @@ class Langevin3D():
     # atomic mass unit
     amu = 1.660538921e-27 #kg 
 
-    def __init__(self, T=None):
+    def __init__(self, T=None, seed=123):
         # radius at equilibrium
-        self.r_0 = 1.275e-10 #m
+        self.r_0 = 1.275 #A #1.275e-10 #m
         # D Morse potential
-        self.D = 4.6141 * 1.60218e-19 #J
+        self.D = 46.141 # (in A) # 4.6141 * 1.60218e-19 #J
         # alpha Morse potential
-        self.alpha = 1.81e10 #m^-1
+        self.alpha = 1.81 #A^-1 1.81e10 #m^-1
 
         # masses
         self.m_Cl = 35.43 * self.amu #kg
@@ -20,16 +20,13 @@ class Langevin3D():
         self.mu = (self.m_Cl * self.m_H) / (self.m_Cl + self.m_H) #kg
 
         self.gamma = 1e14 #s^-1
-        self.k_B = 1.380649e-23 #J/K
+        self.k_B = 1.380649e-3 #(in A) #1.380649e-23 #J/K 
  
-        self.dt = 1 / (self.gamma * 10) #s
+        self.dt = 1 / (self.gamma * 100) #s
 
-        ## if temperature not specified, find temperature with initial speed
-        if T is None:
-            v_init = 300 #m/s
-            self.T = self.mu * v_init**2 / (3 * self.k_B)
-        else:
-            self.T = T
+        self.rng = np.random.default_rng(seed)
+        
+        self.T = T
 
     def potential_Morse(self, r):
         """Morse potential between two atoms at distance r"""
@@ -42,7 +39,7 @@ class Langevin3D():
     def force_Random(self):
         """Random force according to the fluctuation-dissipation theorem"""
         r_amp = np.sqrt(2 * self.k_B * self.T * self.gamma * self.mu / self.dt)
-        return np.random.normal(0, 1) * r_amp
+        return self.rng.normal(0, 1) * r_amp
     
     def compute_force(self, r):
         return -self.force_Morse(r) + self.force_Random()
@@ -60,12 +57,12 @@ class Langevin3D():
         """Langevin dynamics update for a particle"""
         ri_p1 = self._compute_ri(trajectory[-1], trajectory[-2])
         vi = self._compute_vi_verlet(ri_p1, trajectory[-2])
-
         return ri_p1, vi
     
     def langevin(self, trajectory):
+        ri_p1 = self._compute_ri(trajectory[-1], trajectory[-2])
         vi = 3 * trajectory[-1] - 4 * trajectory[-2] + trajectory[-3] / (2 * self.dt)
-        return vi
+        return ri_p1, vi
 
     def run(self, n_steps, r_init, v_init, mode='langevin'):
         """Run the Langevin dynamics simulation for n_steps"""
@@ -81,8 +78,8 @@ class Langevin3D():
         r = r_init
         v = v_init
 
-        trajectory = [r_init, r_init + v_init * self.dt]
-        speed = [v_init, v_init]
+        trajectory = [r_init, r_init + v_init * self.dt, r_init + v_init * 2 * self.dt]
+        speed = [v_init, v_init, v_init]
 
         for _ in range(3, n_steps):
             r, v = alg(trajectory)
